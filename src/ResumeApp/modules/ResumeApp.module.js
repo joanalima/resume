bedc698821b7d86bb74f09e70e604e4a3d2ed1fd
joanalima <joanaduartelima@gmail.com>
2018-09-12 13:25:51 +0200

    json file update location

--- src/ResumeApp/modules/ResumeApp.module.js
+++ src/ResumeApp/modules/ResumeApp.module.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('ResumeApp',['ngMaterial', 'ngMessages'])
-  .constant('ApiPath', '../../../jlresumeinfo.json')
+  .constant('ApiPath', 'https://api.myjson.com/bins/bbkeg')
   .controller('ResumeAppController', ResumeAppController)
   .controller('RecommendationsController', RecommendationsController)
   .controller('SkillsController', SkillsController)
