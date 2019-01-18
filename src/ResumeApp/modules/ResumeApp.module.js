44b5313ed1b494609f225cf779993727e8dfa1dd
joanalima <joanaduartelima@gmail.com>
2019-01-18 11:49:46 +0100

    updating json file

--- src/ResumeApp/modules/ResumeApp.module.js
+++ src/ResumeApp/modules/ResumeApp.module.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('ResumeApp',['ngMaterial', 'ngMessages'])
-  .constant('ApiPath', 'https://api.myjson.com/bins/h0780')
+  .constant('ApiPath', 'https://api.myjson.com/bins/broqo')
   .controller('ResumeAppController', ResumeAppController)
   .controller('RecommendationsController', RecommendationsController)
   .controller('SkillsController', SkillsController)
