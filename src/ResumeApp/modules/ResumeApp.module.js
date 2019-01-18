6f4fb1bfcf7630afddb84caf9ff8d2ed1c734e4c
joanalima <joanaduartelima@gmail.com>
2019-01-18 11:51:40 +0100

    adding .pdf to links

--- src/ResumeApp/modules/ResumeApp.module.js
+++ src/ResumeApp/modules/ResumeApp.module.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('ResumeApp',['ngMaterial', 'ngMessages'])
-  .constant('ApiPath', 'https://api.myjson.com/bins/broqo')
+  .constant('ApiPath', 'https://api.myjson.com/bins/q48f4')
   .controller('ResumeAppController', ResumeAppController)
   .controller('RecommendationsController', RecommendationsController)
   .controller('SkillsController', SkillsController)
